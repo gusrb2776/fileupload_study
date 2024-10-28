b23894019e954515cf848d3853b0a0e474eeb97f
이현규 <gusrb2776@naver.com>
2024-10-28 10:13:07 +0900

    init

---
+++ fileupload_study/src/main/java/com/study/fileupload_study/FileuploadStudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.fileupload_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileuploadStudyApplication.class, args);
+    }
+
+}
